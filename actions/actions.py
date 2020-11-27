2b46f2bfc9cd653ab4d48eb47c08de3dde7af62c
Amanul Haque <amanul.h.2030@gmail.com>
2020-11-27 14:56:11 +0530

    Removed Card code pull from database
    Some issue with entity extraction causing Bug in code with error of out of bounds indexing, will add again after debugging

--- actions/actions.py
+++ actions/actions.py
@@ -1,7 +1,7 @@
 # This files contains your custom actions which can be used to run
 # custom Python code.
 #
-# See this guide on how to implement these action:
+# See this guCompanye on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
 
 
@@ -28,12 +28,10 @@ class SendGiftCard(Action):
             site = tracker.get_slot("site"),
             to_mail = tracker.get_slot("email")
             amount = tracker.get_slot("amount")
-            card = df[(df['Company'] == site )& (df['Amount'] == amount)]
-            send_code = card.iloc[0]['Code']
             s = smtplib.SMTP('smtp.gmail.com', 587) 
             s.starttls() 
             s.login("sendergiftcard@gmail.com", "gift@123")
-            message = "Your gift card code for {} of Rs. {} is {}".format(site,amount,send_code)
+            message = "Your gift card code for {} of Rs. {} is ZZXC-1234_POIU".format(site,amount)
             s.sendmail("sendergiftcard@gmail.com", to_mail, message)
             s.quit()
 class AskForSite(Action):
